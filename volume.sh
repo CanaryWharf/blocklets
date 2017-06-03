dc72f3a700df77969fb223751889d1a564459496
Euston <raja.r184@gmail.com>
2017-06-03 17:19:40 +0100

    made basic scripts

--- volume.sh
+++
@@ -1,7 +0,0 @@
-!#/bin/bash
-if [ $(amixer get Master | grep -c off) == "2" ]
-then
-    echo '<span foreground="#c12505">  </span>'
-else
-    echo '<span foreground="#1793d0">   '"$(amixer get Master | grep -Po "[0-9]+(?=%)" | tail -1)"'</span>'
-fi
